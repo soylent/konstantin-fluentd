b6f5cd48b35a0b5a609a03ea1163cbe3c7bad6e2
Hingst, Arne-Kristian <arne@hingst.it>
2016-01-25 22:19:43 +0100

    Update tdagent.rb
    Fixed missing doublepoint

--- lib/puppet/provider/package/tdagent.rb
+++ lib/puppet/provider/package/tdagent.rb
@@ -1,3 +1,3 @@
-Puppet::Type.type(:package).provide :tdagent, :parent => :gem, :source => gem do
+Puppet::Type.type(:package).provide :tdagent, :parent => :gem, :source => :gem do
   commands :gemcmd => '/opt/td-agent/usr/sbin/td-agent-gem'
 end
