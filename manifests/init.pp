55c12073a4d68216f278f085944f2720daf6c769
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2017-01-28 19:27:20 +0300

    Use Puppet 4 data types
    Please see https://docs.puppet.com/puppet/latest/lang_data_type.html

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,46 +2,28 @@
 # service.
 #
 class fluentd (
-  $repo_install = $::fluentd::params::repo_install,
-  $repo_name = $::fluentd::params::repo_name,
-  $repo_desc = $::fluentd::params::repo_desc,
-  $repo_url = $::fluentd::params::repo_url,
-  $repo_enabled = $::fluentd::params::repo_enabled,
-  $repo_gpgcheck = $::fluentd::params::repo_gpgcheck,
-  $repo_gpgkey = $::fluentd::params::repo_gpgkey,
-  $repo_gpgkeyid = $::fluentd::params::repo_gpgkeyid,
-  $package_name = $::fluentd::params::package_name,
-  $package_ensure = $::fluentd::params::package_ensure,
-  $service_name = $::fluentd::params::service_name,
-  $service_ensure = $::fluentd::params::service_ensure,
-  $service_enable = $::fluentd::params::service_enable,
-  $service_manage = $::fluentd::params::service_manage,
-  $service_provider = $::fluentd::params::service_provider,
-  $config_file = $::fluentd::params::config_file,
-  $config_path = $::fluentd::params::config_path,
-  $config_owner = $::fluentd::params::config_owner,
-  $config_group = $::fluentd::params::config_group,
-  $configs = $::fluentd::params::configs,
-  $plugins = $::fluentd::params::plugins,
+  Boolean $repo_install = $::fluentd::params::repo_install,
+  String $repo_name = $::fluentd::params::repo_name,
+  String $repo_desc = $::fluentd::params::repo_desc,
+  Stdlib::Httpurl $repo_url = $::fluentd::params::repo_url,
+  Boolean $repo_enabled = $::fluentd::params::repo_enabled,
+  Boolean $repo_gpgcheck = $::fluentd::params::repo_gpgcheck,
+  Stdlib::Httpurl $repo_gpgkey = $::fluentd::params::repo_gpgkey,
+  String $repo_gpgkeyid = $::fluentd::params::repo_gpgkeyid,
+  String $package_name = $::fluentd::params::package_name,
+  String $package_ensure = $::fluentd::params::package_ensure,
+  String $service_name = $::fluentd::params::service_name,
+  String $service_ensure = $::fluentd::params::service_ensure,
+  Boolean $service_enable = $::fluentd::params::service_enable,
+  Boolean $service_manage = $::fluentd::params::service_manage,
+  Optional[String] $service_provider = $::fluentd::params::service_provider,
+  Stdlib::Absolutepath $config_file = $::fluentd::params::config_file,
+  Stdlib::Absolutepath $config_path = $::fluentd::params::config_path,
+  String $config_owner = $::fluentd::params::config_owner,
+  String $config_group = $::fluentd::params::config_group,
+  Hash $configs = $::fluentd::params::configs,
+  Hash $plugins = $::fluentd::params::plugins,
 ) inherits fluentd::params {
-
-  validate_bool($repo_install)
-  validate_string($repo_name)
-  validate_string($repo_url)
-  validate_bool($repo_enabled)
-  validate_bool($repo_gpgcheck)
-  validate_string($repo_gpgkey)
-  validate_string($repo_gpgkeyid)
-  validate_string($package_name)
-  validate_string($package_ensure)
-  validate_string($service_name)
-  validate_string($service_ensure)
-  validate_bool($service_enable)
-  validate_bool($service_manage)
-  validate_absolute_path($config_file)
-  validate_string($config_owner)
-  validate_string($config_group)
-
   contain fluentd::install
   contain fluentd::service
 
